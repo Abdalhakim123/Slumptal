4c2bfb8235470ce2dd33f4ee46d8dcacc3d637cc
Abdalhakim123 <70113116+Abdalhakim123@users.noreply.github.com>
2020-11-18 14:57:53 +0100

    Add files via upload

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen");
+            Console.WriteLine("Gissa talet, lycka till");
+
+            int nr;
+            string strNr;
+            Random randomerare = new Random();
+            int slump_tal = randomerare.Next(1, 101);
+            strNr = Console.ReadLine();
+            nr = Convert.ToInt32(strNr);
+            int guess = 1;
+
+
+            do
+            {
+
+
+                guess++;
+
+
+                if (nr > slump_tal)
+                {
+                    Console.WriteLine("Tyvärr fel nummer");
+                    Console.WriteLine("Talet är mindre, gissa igen");
+                    strNr = Console.ReadLine();
+                    nr = Convert.ToInt32(strNr);
+                }
+                else if (nr < slump_tal)
+                {
+                    Console.WriteLine("Tyvärr fel nummer");
+                    Console.WriteLine("Talet är större, gissa igen");
+                    strNr = Console.ReadLine();
+                    nr = Convert.ToInt32(strNr);
+                }
+
+                if (nr == slump_tal)
+                {
+                    Console.WriteLine("Grattis du gissade nummret efter " + guess + " gissningar");
+                    Console.ReadLine();
+
+
+                }
+
+            } while (nr != slump_tal);
+        }
+    }
+}
